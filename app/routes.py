dc99ec8a9d71b54f3e370ce9e29fb5c2c2392340
Nathan Ruiz <natruiz3553@gmail.com>
2020-04-29 11:37:06 +0000

    Add 'bind_game' decorator to flask routes

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,46 @@
 import io
 import datetime
+from functools import wraps
 from flask import send_file, render_template, redirect, url_for, request
 from app import app, db
 from app.game.game import Game
 from app.game.generate import generate_map, generate_market
 
+def bind_game(save=False):
+    """A decorator that builds a game object from the database, and passes it
+    in to the decorated function using the 'game' parameter.
+    """
+    def outer(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            game_id = kwargs["game_id"]
+            state = db.load_game_state(game_id)
+            game = Game(state)
+            result = func(*args, **kwargs, game=game)
+
+            if save:
+                db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
+
+            return result
+        return inner
+    return outer
+
+
 @app.route('/game/<game_id>/map')
-def map_image(game_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game()
+def map_image(game, game_id):
     image = io.BytesIO(generate_map(game))
     return send_file(image, mimetype="image/svg+xml")
 
 @app.route('/game/<game_id>/market')
-def market(game_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game()
+def market(game, game_id):
     data = io.BytesIO(generate_market(game).encode("UTF-8"))
     return send_file(data, mimetype="image/svg+xml")
 
 @app.route('/game/<game_id>/view')
-def view(game_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game()
+def view(game, game_id):
     # Add things to help game separate logic
     for i in game.companies:
         game.companies[i].token_img = url_for('static_assets', path=f'tokens/{i}.svg')
@@ -48,78 +66,60 @@ def create_game():
 
 # Game play
 @app.route('/game/<game_id>/pa/pass')
-def pa_pass(game_id, game=None):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def pa_pass(game, game_id):
     game.act_pa_pass()
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 @app.route('/game/<game_id>/pa/buy')
-def pa_buy(game_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def pa_buy(game, game_id):
     game.act_pa_buy()
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 @app.route('/game/<game_id>/pa/bid/<private_id>', methods=['POST'])
-def pa_bid(game_id, private_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def pa_bid(game, game_id, private_id):
     bid = int(request.form.get("bid_amt"))
     game.act_pa_bid(bid, private_id)
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 @app.route('/game/<game_id>/<company_id>/start/', methods=['POST'])
-def sr_buy_president(game_id, company_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def sr_buy_president(game, game_id, company_id):
     ipo_price = int(request.form.get('president_price'))
     game.act_sr_buy_president(company_id, ipo_price)
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 @app.route('/game/<game_id>/<company_id>/buy_ipo/', methods=['POST'])
-def sr_buy_ipo(game_id, company_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def sr_buy_ipo(game, game_id, company_id):
     game.act_sr_buy_ipo(company_id)
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 @app.route('/game/<game_id>/<company_id>/buy_market/', methods=['POST'])
-def sr_buy_market(game_id, company_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def sr_buy_market(game, game_id, company_id):
     game.act_sr_buy_market(company_id)
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 @app.route('/game/<game_id>/<company_id>/sell/', methods=['POST'])
-def sr_sell(game_id, company_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def sr_sell(game, game_id, company_id):
     amount = int(request.form.get('qty'))
     game.act_sr_sell(company_id, amount)
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 @app.route('/game/<game_id>/sr_pass/', methods=['POST'])
-def sr_pass(game_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def sr_pass(game, game_id):
     game.act_sr_pass()
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 @app.route('/game/<game_id>/sr_done/', methods=['POST'])
-def sr_done(game_id):
-    state = db.load_game_state(game_id)
-    game = Game(state)
+@bind_game(save=True)
+def sr_done(game, game_id):
     game.act_sr_done()
-    db.save_game_state(game_id, game.get_state(), datetime.datetime.now())
     return redirect(url_for("view", game_id=game_id))
 
 
